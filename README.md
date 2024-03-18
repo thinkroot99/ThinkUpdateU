67549c138fe5170c9ab68932974f35676edeeb4b
ThinkRoot99 <social@crism.ro>
2024-03-18 12:05:49 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ThinkUpdateU
- Script pentru actualizare sistemului Ubuntu
+
+### Descriere:
+- Acest script permite actualizarea pachetelor DEB, Flatpak și Snap pe un sistem Linux. Puteți alege să actualizați fiecare tip de pachet în mod individual sau să actualizați toate pachetele deodată. 
+
+### Utilizare
+1. Deschide terminalul și navighează în directorul în care ai salvat scriptul.
+2. Acordă permisiuni de executare pentru script folosind comanda `chmod +x ThinkUpdateU.sh`.
+3. Rulează scriptul folosind comanda `./ThinkUpdateU.sh`.
--- ThinkUpdateU.sh
+++ ThinkUpdateU.sh
@@ -6,12 +6,12 @@
 # Versiune: 1.1
 
 # Descriere:
-#   Acest script permite actualizarea pachetelor DEB, Flatpak și Snap pe un sistem Linux. Puteți alege să actualizați fiecare tip de pachet în mod individual sau să actualizați toate pachetele deodată. 
+#   Acest script permite actualizarea pachetelor DEB, Flatpak și Snap pe un sistem Ubuntu. Puteți alege să actualizați fiecare tip de pachet în mod individual sau să actualizați toate pachetele deodată. 
 
 # Utilizare
 # 1. Deschide terminalul și navighează în directorul în care ai salvat scriptul.
 # 2. Acordă permisiuni de executare pentru script folosind comanda: chmod +x ThinkUpdateU.sh.
-# 2. Rulează scriptul folosind comanda: ./ThinkUpdateU.sh.
+# 3. Rulează scriptul folosind comanda: ./ThinkUpdateU.sh.
 ##########################################################################################################################
 
 # Funcție pentru a afișa meniul
