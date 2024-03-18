fdd07e02e0d0c4b67afddab3bd119b4315fcb5b9
ThinkRoot99 <social@crism.ro>
2024-03-18 12:03:46 +0200

    Update ThinkUpdateU.sh

--- ThinkUpdateU.sh
+++ ThinkUpdateU.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##########################################################################################################################
-# Script: ThinkInstallF
+# Script: ThinkUpdateU
 # Autor: ThinkRoot
 # Versiune: 1.1
 
